f62acc704f2a8155f5899acc98828831ba3d0123
jessluo8 <jessie.luo@utdallas.edu>
2019-03-17 17:07:53 -0500

    Alarm added

--- app/src/main/java/com/example/myapplication/PomodoroActivity.java
+++ app/src/main/java/com/example/myapplication/PomodoroActivity.java
@@ -8,17 +8,19 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.os.CountDownTimer;
 import java.util.concurrent.TimeUnit;
+import android.media.MediaPlayer;
 
 public class PomodoroActivity extends AppCompatActivity {
 
     public static final long MILLI = 1000; // 1000 milliseconds in one second
-    public static final long WORK = 1500000; // 25 minutes
-    public static final long LONG_BREAK = 1800000; // 30 minutes
-    public static final long SHORT_BREAK = 300000; // 5 minutes
+    public static final long WORK = 15000; // 25 minutes
+    public static final long LONG_BREAK = 18000; // 30 minutes
+    public static final long SHORT_BREAK = 3000; // 5 minutes
 
     CountDownTimer countdown;
     private Button startButton;
     private Button pauseButton;
+    private Button stopButton;
 
     private TextView mins;
     private TextView secs;
@@ -29,6 +31,8 @@ public class PomodoroActivity extends AppCompatActivity {
     private long timeLeft;
     boolean isPaused;
 
+    private MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +40,7 @@ public class PomodoroActivity extends AppCompatActivity {
 
         startButton = (Button) findViewById(R.id.start_countdown);
         pauseButton = (Button) findViewById(R.id.pause_countdown);
+        stopButton = (Button) findViewById(R.id.stop_alarm);
 
         mins = (TextView)findViewById(R.id.minutes);
         secs = (TextView)findViewById(R.id.seconds);
@@ -75,6 +80,7 @@ public class PomodoroActivity extends AppCompatActivity {
         countdown = new CountDownTimer(startTime, MILLI) {
             @Override
             public void onTick(long millisUntilFinished) {
+                mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.alarm);
                 isPaused = false;
                 startButton.setVisibility(View.GONE);
                 pauseButton.setVisibility(View.VISIBLE);
@@ -86,26 +92,12 @@ public class PomodoroActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
+                mediaPlayer.start();
+                mediaPlayer.setLooping(true);
                 isPaused = false;
-                if(startTime == WORK) {
-                    if(count == -1) {
-                        workState.setText("Long Break Time");
-                        mins.setText("30");
-                        secs.setText("00");
-                    }
-                    else {
-                        workState.setText("Short Break Time");
-                        mins.setText("5");
-                        secs.setText("00");
-                    }
-                }
-                else {
-                    workState.setText("Work Time");
-                    mins.setText("25");
-                    secs.setText("00");
-                }
                 pauseButton.setVisibility(View.GONE);
-                startButton.setVisibility(View.VISIBLE);
+                stopButton.setVisibility(View.VISIBLE);
+                workState.setText("CONGRATULATIONS YOU MADE IT");
             }
         }.start();
     }
@@ -122,5 +114,40 @@ public class PomodoroActivity extends AppCompatActivity {
         startButton.setVisibility(View.VISIBLE);
         startTime = timeLeft;
         isPaused = true;
+    }
+
+    public void stopAlarm(View view) {
+        stopButton.setVisibility(View.GONE);
+        startButton.setVisibility(View.VISIBLE);
+        mediaPlayer.stop();
+        if(startTime == WORK) {
+            if(count == -1) {
+                workState.setText("Long Break Time");
+                mins.setText("30");
+                secs.setText("00");
+            }
+            else {
+                workState.setText("Short Break Time");
+                mins.setText("5");
+                secs.setText("00");
+            }
+        }
+        else {
+            workState.setText("Work Time");
+            mins.setText("25");
+            secs.setText("00");
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+        }
+        else {
+            countdown.cancel();
+        }
+        //countdown.cancel();
+        super.onBackPressed();
     }
 }
