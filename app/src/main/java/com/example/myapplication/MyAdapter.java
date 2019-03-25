8f4607a2a43c2f48947bc43fa53cf9f868380c05
ejlam <emily.lam.21@gmail.com>
2019-03-24 21:29:35 -0500

    RecyclerView works, task list works, created toolbar, need to add menu buttons to toolbar, need make an add task page and link it to button

--- app/src/main/java/com/example/myapplication/MyAdapter.java
+++ app/src/main/java/com/example/myapplication/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
