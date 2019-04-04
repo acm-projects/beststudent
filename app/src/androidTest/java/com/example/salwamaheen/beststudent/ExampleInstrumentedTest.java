e0c2bd9d54c6b5ae5197102566aa00287651b179
salwamaheen <salwamaheen@cometnet-10-21-75-176.utdallas.edu>
2019-04-03 21:23:45 -0500

    fuck

--- app/src/androidTest/java/com/example/salwamaheen/beststudent/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/salwamaheen/beststudent/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.salwamaheen.beststudent;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.myapplication", appContext.getPackageName());
+        assertEquals("com.example.salwamaheen.beststudent", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/example/salwamaheen/beststudent/ExampleUnitTest.java
+++ app/src/test/java/com/example/salwamaheen/beststudent/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.salwamaheen.beststudent;
 
 import org.junit.Test;
 
